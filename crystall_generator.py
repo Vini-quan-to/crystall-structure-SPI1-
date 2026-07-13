38c40cdae4f66f43a10e0538b61c2dd029a7d83b
Vini-quan-to <gvinit621@gmail.com>
2026-07-13 10:47:35 +0530

    added the new file for the surface and made the code efficient by thr introduction of the function to it

--- crystall_generator.py
+++ crystall_generator.py
@@ -33,25 +33,61 @@ n3=int(input("enter the number of lattice points in  the z direction:"))
 print("Enter your lattice vector ")
 
 # INPUT FOR THE LATTICE VECTOR IN THE X Y Z DIRECTION
-for i in range(3):
-    ax=int(input("enter the x-direction of your lattice vector:  "))
-    ay=int(input("enter the y-direction of your lattice vector:  "))
-    az=int(input("enter the z-direction of your lattice vector:  "))
-    a1=np.array([ax,ay,az])
-    print(f'your given lattice vector {a1} is stored in the system')
-    cl.append(a1)
+print("""=========================================================================================================""")
+print(" NOTE:  For The  conventional lattice enter (con) and for the particular or custom lattice enter (cus)")
+print("""=========================================================================================================""")
+
+custom_conventional = input(" Enter the type of lattice you want conventional or custom:")
+
+if custom_conventional=="con":
+    a1=np.array([1,0,0])
+    a2=np.array([0,1,0])
+    a3=np.array([0,0,1])
+    cl.extend([a1,a2,a3])
+else:
+    for i in range(3):
+        ax=int(input("enter the x-direction of your lattice vector:  "))
+        ay=int(input("enter the y-direction of your lattice vector:  "))
+        az=int(input("enter the z-direction of your lattice vector:  "))
+        a1=np.array([ax,ay,az])
+        print(f'your given lattice vector {a1} is stored in the system')
+        cl.append(a1)
+        
+
 print(f' These are all the lattice vector given as input {cl}')
 
-# INPUT FOR THE BASIS VECTOR IN THE X Y Z DIRECTION
+print("""=========================================================================================================""")
+print("NOTE: for the simple cubic enter(SC ) ,for body centered enter (BCC) , Face centered enter (FCC) and for something else enter (0) ")
+print("""=========================================================================================================""")
 
-number_basis=int(input("enter the number of basis atom in the crystall:"))
-for i in range(number_basis):
-    bx=float(input("enter the x-direction of the basis vector:"))
-    by=float(input("enter the y-direction of the  basis vector:"))
-    bz=float(input("enter the z-direction of  the basis vector:"))
-    b1=np.array([bx,by,bz])
-    print(f'your given basis {b1} is stored in the system')
+
+SC_BCC_FCC_O=input("Enter the type of cell you wnat in the lattice based on the note just given above :")
+if SC_BCC_FCC_O=="SC":
+    b1=np.array([0,0,0])
     basis.append(b1)
+
+elif SC_BCC_FCC_O=="BCC":
+    b1=np.array([0,0,0])
+    b2=np.array([.5,.5,.5])
+    basis.extend([b1 ,b2])
+
+elif SC_BCC_FCC_O=="FCC":
+    b1=np.array([0,0,0])
+    b2=np.array([0.5,0.5,0])
+    b3=np.array([0.5,0,0.5])
+    b4=np.array([0,0.5,0.5])
+    basis.extend([b1,b2,b3,b4])
+
+elif SC_BCC_FCC_O=="0":
+    number_basis=int(input("enter the number of basis atom in the crystall:"))
+    for i in range(number_basis):
+        bx=float(input("enter the x-direction of the basis vector:"))
+        by=float(input("enter the y-direction of the  basis vector:"))
+        bz=float(input("enter the z-direction of  the basis vector:"))
+        b1=np.array([bx,by,bz])
+        print(f'your given basis {b1} is stored in the system')
+        basis.append(b1)
+
 print(f' These are all the basis given as input {basis}')
 
 # INPUT FOR THE MILLER INDICES OF THE PLANES IN THE CRYSTALL
@@ -62,6 +98,7 @@ l=int(input("enter the miller  index  l :"))
 print(f' The given miller indices are {h} {k} {l} and the plane is {h} {k} {l}')
 
 # enter the number of layers to see the normal view of the crystall plane
+
 number_layers=int(input("enter the number of layers to see the normal view of the crystall plane:"))
 
 # USE THE FORMULA TO CALCULATE THE RECIPROCAL LATTICE VECTOR
@@ -75,111 +112,209 @@ b1=(2*np.pi*z)/v
 b2=(2*np.pi*(np.cross(a3,a1)))/v
 b3=(2*np.pi*(np.cross(a1,a2)))/v
 
-rlv.append(b1)
-rlv.append(b2)
-rlv.append(b3)
+rlv.extend([b1 ,b2 ,b3])
+
 print(f" this are  your reciprocal lattice vector formed from the  given primitibe attice vector {rlv}")
 
-# Condtion used for  the lattice point generation  with the given basis as well
-for j in range(number_basis):
-    for n in range(n1):
-        for m in range(n2):
-            for p in range(n3):
+print("""=========================================================================================================""")
+print("NOTE: for just to  crystall(lattice + basis) enter the  (CRY) and for only normal veiw (NV) for both  crsytall and the normal view (BOTH)")
+print("""=========================================================================================================""")
 
-                r=(n*cl[0] + m*cl[1] + p*cl[2])
-                lattice_x.append(r[0])
-                lattice_y.append(r[1])
-                lattice_z.append(r[2])       
+#=====================================================================================================================================================================================================================================================================
+
+def crystall(n1,n2 ,n3 ,cl,basis):
+    for j in range(number_basis):
+        for n in range(n1):
+            for m in range(n2):
+                for p in range(n3):
+
+                    r=(n*cl[0] + m*cl[1] + p*cl[2])
+                    lattice_x.append(r[0])
+                    lattice_y.append(r[1])
+                    lattice_z.append(r[2])       
+                    
+                    r1=(n*cl[0] + m*cl[1] + p*cl[2]) + basis[j]
+                    basis1_x.append(r1[0])
+                    basis1_y.append(r1[1])
+                    basis1_z.append(r1[2])
+
+    fig = mp.figure(figsize=(12,5))
+
+    ax1 = fig.add_subplot(121, projection="3d")
+
+    ax1.scatter(basis1_x,basis1_y,basis1_z, s=70, color="red",label="atoms")
+
+    ax1.scatter(lattice_x,lattice_y,lattice_z,s=70,color="blue",label="lattice")
+
+    ax1.set_title("Complete Crystal")
+
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    ax1.set_zlabel("z")
+
+    ax1.legend()
+    mp.show()
+
+#=====================================================================================================================================================================================================================================================================
+
+
+def normal_view(n1,n2, n3, number_basis,cl,basis ,h,k,l):
+    for j in range(number_basis):
+        for n in range(n1):
+            for m in range(n2):
+                for p in range(n3):
+
+                    r=(n*cl[0] + m*cl[1] + p*cl[2])
+                    lattice_x.append(r[0])
+                    lattice_y.append(r[1])
+                    lattice_z.append(r[2])       
+                    
+                    r1=(n*cl[0] + m*cl[1] + p*cl[2]) + basis[j]
+                    basis1_x.append(r1[0])
+                    basis1_y.append(r1[1])
+                    basis1_z.append(r1[2])
+                    g=(h*b1 + k*b2 + l*b3)
+            
+                    d=np.dot(g,r1)
+
+                    d=round(d,5)
+                                                                                        #   this part of the code is used to make list of the atoms belonging to a particulatr layers
+                    if d not in layers:
+                        layers[d]=[]
+
+                    layers[d].append(r1)
+            
+    for key,value in layers.items():
+        print("layer:",key)
+        print(value)
+
+    print(layers.keys())
+
+    layer_numbers = sorted(layers.keys())
+    normal = g / np.linalg.norm(g)
+    random=np.array([1,0,0])
+
+    u=np.cross(normal,random)
+
+    u=u/np.linalg.norm(u)
+
+    v=np.cross(normal,u)
+
+    v=v/np.linalg.norm(v)
+    fig = mp.figure(figsize=(12,5))
+
+    ax1 = fig.add_subplot(121, projection="3d")
+
+
+    for i in range(number_layers):
+
+        layer_atoms=np.array(layers[layer_numbers[i]])
+
+        X=[]
+        Y=[]
+
+
+        for atom in layer_atoms:
+
+            x_new=np.dot(atom,u)
+            y_new=np.dot(atom,v)
+
+            X.append(x_new)
+            Y.append(y_new)
+
+            ax.scatter( X, Y, s=70, label=f"layer {i}" )
+
+    ax.set_title(f"({h}{k}{l}) surface view")
+    mp.show()
+
+#=====================================================================================================================================================================================================================================================================
+
+
+def crystall_surface_view(n1,n2, n3, number_basis,cl,basis , h , k , l ):
+    for j in range(number_basis):
+         for n in range(n1):
+             for m in range(n2):
+                for p in range(n3):
+
+                    r=(n*cl[0] + m*cl[1] + p*cl[2])
+                    lattice_x.append(r[0])
+                    lattice_y.append(r[1])
+                    lattice_z.append(r[2])       
+                    
+                    r1=(n*cl[0] + m*cl[1] + p*cl[2]) + basis[j]
+                    basis1_x.append(r1[0])
+                    basis1_y.append(r1[1])
+                    basis1_z.append(r1[2])
+                    g=(h*b1 + k*b2 + l*b3)
                 
-                r1=(n*cl[0] + m*cl[1] + p*cl[2]) + basis[j]
-                basis1_x.append(r1[0])
-                basis1_y.append(r1[1])
-                basis1_z.append(r1[2])
-                g=(h*b1 + k*b2 + l*b3)
-            
-                d=np.dot(g,r1)
+                    d=np.dot(g,r1)
 
-                d=round(d,5)
-                                                                                    #   this part of the code is used to make list of the atoms belonging to a particulatr layers
-                if d not in layers:
-                    layers[d]=[]
+                    d=round(d,5)
+                                                                                        #   this part of the code is used to make list of the atoms belonging to a particulatr layers
+                    if d not in layers:
+                        layers[d]=[]
 
-                layers[d].append(r1)
-            
+                    layers[d].append(r1)
+                
 
-for key,value in layers.items():
-    print("layer:",key)
-    print(value)
+    for key,value in layers.items():
+        print("layer:",key)
+        print(value)
 
-print(layers.keys())
+    print(layers.keys())
 
+    fig = mp.figure(figsize=(12,5))
 
-# PLOTTING PART
+    ax1 = fig.add_subplot(121, projection="3d")
 
+    ax1.scatter(basis1_x,basis1_y,basis1_z, s=70, color="red",label="atoms")
 
-fig = mp.figure(figsize=(12,5))
+    ax1.scatter(lattice_x,lattice_y,lattice_z,s=70,color="blue",label="lattice")
 
-ax1 = fig.add_subplot(121, projection="3d")
+    ax1.set_title("Complete Crystal")
 
-ax1.scatter(basis1_x,basis1_y,basis1_z, s=70, color="red",label="atoms")
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    ax1.set_zlabel("z")
 
-ax1.scatter(lattice_x,lattice_y,lattice_z,s=70,color="blue",label="lattice")
+    ax1.legend()
 
-ax1.set_title("Complete Crystal")
+    layer_numbers = sorted(layers.keys())
+    normal = g / np.linalg.norm(g)
+    random=np.array([1,0,0])
 
-ax1.set_xlabel("x")
-ax1.set_ylabel("y")
-ax1.set_zlabel("z")
+    u=np.cross(normal,random)
 
-ax1.legend()
+    u=u/np.linalg.norm(u)
 
-#  SURFACE VIEW ALONG MILLER NORMAL
+    v=np.cross(normal,u)
 
-layer_numbers = sorted(layers.keys())
-normal = g / np.linalg.norm(g)
-random=np.array([1,0,0])
+    v=v/np.linalg.norm(v)
 
-u=np.cross(normal,random)
-
-u=u/np.linalg.norm(u)
-
-v=np.cross(normal,u)
-
-v=v/np.linalg.norm(v)
-
-ax2 = fig.add_subplot(122)
+    ax2 = fig.add_subplot(122)
 
 
-for i in range(number_layers):
+    for i in range(number_layers):
 
-    layer_atoms=np.array(layers[layer_numbers[i]])
+        layer_atoms=np.array(layers[layer_numbers[i]])
 
-    X=[]
-    Y=[]
-
-
-    for atom in layer_atoms:
-
-        x_new=np.dot(atom,u)
-        y_new=np.dot(atom,v)
-
-        X.append(x_new)
-        Y.append(y_new)
+        X=[]
+        Y=[]
 
 
+        for atom in layer_atoms:
 
-    ax2.scatter( X, Y, s=70, label=f"layer {i}" )
+            x_new=np.dot(atom,u)
+            y_new=np.dot(atom,v)
 
+            X.append(x_new)
+            Y.append(y_new)
 
-ax2.set_title(f"({h}{k}{l}) surface view")
+        ax2.scatter( X, Y, s=70, label=f"layer {i}" )
 
+    ax2.set_title(f"({h}{k}{l}) surface view")
 
+    ax2.legend()
 
-ax2.legend()
-
-mp.show()
-
-
-
+    mp.show()
     
-
